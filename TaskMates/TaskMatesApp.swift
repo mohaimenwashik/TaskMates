1fc84be928904fceda129cd107d49525655a3d20
Mohaimen Washik Rahman <mrahma62@asu.edu>
2024-04-14 04:25:29 -0700

    Initial Commit

---
+++ TaskMates/TaskMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMatesApp.swift
+//  TaskMates
+//
+//  Created by Mohaimen Washik on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
